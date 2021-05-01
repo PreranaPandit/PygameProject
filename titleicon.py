7b391311888a13073655d3758e5a23381009bcd6
PreranaPandit <preranapandit07@gmail.com>
2021-05-01 20:19:32 +0545

    Initial commit

---
+++ titleicon.py
@@ -0,0 +1,25 @@
+import pygame
+
+# initialize pygame
+pygame.init()
+
+# Create the screen
+screen = pygame.display.set_mode((800,600))
+
+# title and icon
+pygame.display.set_caption("SPACE INVADERS")
+icon = pygame.image.load('project.png')
+pygame.display.set_icon(icon)
+
+# Game loop
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        # RGB COLOR TO RGB for background color
+        screen.fill((108,0,58))
+        pygame.display.update()
+
